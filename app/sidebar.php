55f178b9856e46b5069fc396a7912962f7ab581d
safrizalrahman46 <safrizalrahman46@gmail.com>
2024-11-15 14:56:54 +0800

    Push safrizal rahman autentikasi

--- app/sidebar.php
+++ app/sidebar.php
@@ -6,7 +6,7 @@
           <img src="dist/img/user2-160x160.jpg" class="img-circle elevation-2" alt="User Image">
         </div>
         <div class="info">
-          <a href="#" class="d-block"><?php echo $_SESSION['nama'];?></a>
+          <a href="#" class="d-block"><?php echo $_SESSION['nama'].' | '.$_SESSION['level'];?></a>
         </div>
       </div>
 
--- conf/autentikasi.php
+++ conf/autentikasi.php
@@ -8,8 +8,10 @@ $query = mysqli_query($koneksi, "SELECT * FROM users WHERE username='$username' 
 
 if (mysqli_num_rows($query) == 1) {
     header('Location: ../app');
-    $_SESSION['nama'] = 'heri';
-    $_SESSION['level'] = 'superadmin';
+
+    $user = mysqli_fetch_array($query);
+    $_SESSION['nama'] = $user['nama'];
+    $_SESSION['level'] = $user['level'];
 } 
  else if ($username == '' || $password == '') {
     header('Location: ../index.php?error=2');
