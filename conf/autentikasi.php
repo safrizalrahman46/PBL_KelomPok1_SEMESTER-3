8ab8980c60179596fc1d9e229abb3f660e96272d
safrizalrahman46 <safrizalrahman46@gmail.com>
2024-11-15 13:25:21 +0800

    Push safrizal rahman fungsi echo nama

--- conf/autentikasi.php
+++ conf/autentikasi.php
@@ -1,4 +1,5 @@
 <?php
+session_abort();
 include ('config.php');
 $username = $_POST['username'];
 $password = $_POST['password'];
@@ -8,6 +9,9 @@ $query = mysqli_query($koneksi, "SELECT * FROM users WHERE username='$username' 
 if (mysqli_num_rows($query) == 1) {
     // Redirect to login page with success parameter
     header('Location:../index.php?success=1');
+    $_SESSION['nama'] = 'admin';
+    $_SESSION['levle'] = 'superadmin';
+
 } else if ($username == '' || $password == '') {
     header('Location: ../index.php?error=2');
 } else {
