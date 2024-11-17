a928b010ce7671a6e980a26c8fbf35471879dce1
safrizalrahman46 <safrizalrahman46@gmail.com>
2024-11-17 21:34:40 +0800

    Delete Function 1

--- app/data_mahasiswa.php
+++ app/data_mahasiswa.php
@@ -60,7 +60,7 @@
                     <td><?php echo $mhs['total_reward_points']; ?></td>
                     <td><?php echo $mhs['semester']; ?></td>
                     <td><?php echo $mhs['tingkat']; ?></td>
-                    <td><a href="delete/hapus_data.php?id=<?php echo $mhs['id']; ?>" class="btn btn-sm btn-danger">Hapus</a></td>
+                    <td><a onclick="hapus_data()"  class="btn btn-sm btn-danger">Hapus</a></td>
 
 
                   </tr>
