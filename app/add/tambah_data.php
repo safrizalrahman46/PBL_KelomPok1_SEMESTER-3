5e36e4f812c134550fac1ca5e5af16142eda664c
safrizalrahman46 <safrizalrahman46@gmail.com>
2024-11-17 17:59:35 +0800

    CRUD #1 Tambah coba

--- app/add/tambah_data.php
+++ app/add/tambah_data.php
@@ -1,3 +1,36 @@
 <?php
-echo $_GET['nama'];
+
+// echo "Name: " . $name . "<br>";
+// echo "Department ID: " . $department_id . "<br>";
+// echo "Email: " . $email . "<br>";
+// echo "NIM: " . $NIM . "<br>";
+// echo "Username: " . $username . "<br>";
+// echo "Password: " . $password . "<br>";
+// echo "Total Violation Points: " . $total_violation_points . "<br>";
+// echo "Total Reward Points: " . $total_reward_points . "<br>";
+// echo "Semester: " . $semester . "<br>";
+// echo "Tingkat: " . $tingkat . "<br>";
+
+echo = $_GET['name'];
+echo = $_GET['department_id'];
+echo = $_GET['email'];
+echo = $_GET['NIM'];
+echo = $_GET['username'];
+echo = $_GET['password'];
+echo = $_GET['total_violation_points'];
+echo = $_GET['total_reward_points'];
+echo = $_GET['semester'];
+echo = $_GET['tingkat'];
+
+
+// $name = $_GET['name'];
+// $department_id = $_GET['department_id'];
+// $email = $_GET['email'];
+// $NIM = $_GET['NIM'];
+// $username = $_GET['username'];
+// $password = $_GET['password'];
+// $total_violation_points = $_GET['total_violation_points'];
+// $total_reward_points = $_GET['total_reward_points'];
+// $semester = $_GET['semester'];
+// $tingkat = $_GET['tingkat'];
 ?>
