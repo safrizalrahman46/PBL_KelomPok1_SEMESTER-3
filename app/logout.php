7b0a9ef3145051d60c71df9b9d8dd618afbf9c95
safrizalrahman46 <safrizalrahman46@gmail.com>
2024-11-17 17:33:56 +0800

    push logout 100%

--- app/logout.php
+++ app/logout.php
@@ -1,6 +1,7 @@
 <?php
 session_start();
-unset($_SESSION['nama']);
-unset($_SESSION['level']);
+// unset($_SESSION['nama']);
+// unset($_SESSION['level']);
+session_destroy();
 header('Location: ../');
 ?>
