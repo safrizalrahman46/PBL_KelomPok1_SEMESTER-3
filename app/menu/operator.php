50bd48546449716a92fb7b2b071b59fa18237ef3
safrizalrahman46 <safrizalrahman46@gmail.com>
2024-11-17 16:52:35 +0800

    push database update2

--- app/menu/operator.php
+++ app/menu/operator.php
@@ -1,148 +1,156 @@
 <nav class="mt-2">
-        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-          <!-- Add icons to the links using the .nav-icon class
+  <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+    <!-- Add icons to the links using the .nav-icon class
                with font-awesome or any other icon font library -->
-               <li class="nav-item menu-open">
-    <a href="index.php?page=dashboard" class="nav-link active">
+    <li class="nav-item menu-open">
+      <a href="index.php?page=dashboard" class="nav-link ">
         <i class="nav-icon fas fa-tachometer-alt"></i>
         <p>
-            Dashboard
-            <i class=""></i>
+          Dashboard
+          <i class=""></i>
         </p>
-    </a>
-</li>
-<li class="nav-item">
-    <a href="index.php?page=data-masterdata" class="nav-link">
+      </a>
+    </li>
+    <li class="nav-item">
+      <a href="index.php?page=data-masterdata" class="nav-link">
         <i class="nav-icon fas fa-database"></i>
         <p>
-            Master Data
-            <i class="right fas fa-angle-left"></i>
+          Master Data
+          <i class="right fas fa-angle-left"></i>
         </p>
-    </a>
-    <ul class="nav nav-treeview">
+      </a>
+      <ul class="nav nav-treeview">
         <li class="nav-item">
-            <a href="./admin_task_log.html" class="nav-link">
-                <i class="far fa-circle nav-icon"></i>
-                <p>Admin Task Log</p>
-            </a>
+          <a href="./admin_task_log.html" class="nav-link">
+            <i class="far fa-circle nav-icon"></i>
+            <p>Admin Task Log</p>
+          </a>
         </li>
         <li class="nav-item">
-            <a href="index.php?page=data-department" class="nav-link">
-                <i class="far fa-circle nav-icon"></i>
-                <p>Department</p>
-            </a>
+          <a href="index.php?page=data-department" class="nav-link">
+            <i class="far fa-circle nav-icon"></i>
+            <p>Department</p>
+          </a>
         </li>
         <li class="nav-item">
-            <a href="index.php?page=data-dosen" class="nav-link">
-                <i class="far fa-circle nav-icon"></i>
-                <p>Dosen</p>
-            </a>
+          <a href="index.php?page=data-dosen" class="nav-link">
+            <i class="far fa-circle nav-icon"></i>
+            <p>Dosen</p>
+          </a>
         </li>
         <li class="nav-item">
-            <a href="index.php?page=data-dpa" class="nav-link">
-                <i class="far fa-circle nav-icon"></i>
-                <p>DPA</p>
-            </a>
+          <a href="index.php?page=data-dpa" class="nav-link">
+            <i class="far fa-circle nav-icon"></i>
+            <p>DPA</p>
+          </a>
         </li>
         <li class="nav-item">
-            <a href="./fakultas.html" class="nav-link">
-                <i class="far fa-circle nav-icon"></i>
-                <p>Fakultas</p>
-            </a>
+          <a href="./fakultas.html" class="nav-link">
+            <i class="far fa-circle nav-icon"></i>
+            <p>Fakultas</p>
+          </a>
         </li>
         <li class="nav-item">
-            <a href="./kelas.html" class="nav-link">
-                <i class="far fa-circle nav-icon"></i>
-                <p>Kelas</p>
-            </a>
+          <a href="./kelas.html" class="nav-link">
+            <i class="far fa-circle nav-icon"></i>
+            <p>Kelas</p>
+          </a>
         </li>
         <li class="nav-item">
-            <a href="./komisi_discipline_decision.html" class="nav-link">
-                <i class="far fa-circle nav-icon"></i>
-                <p>Komisi Discipline Decision</p>
-            </a>
+          <a href="./komisi_discipline_decision.html" class="nav-link">
+            <i class="far fa-circle nav-icon"></i>
+            <p>Komisi Discipline Decision</p>
+          </a>
         </li>
         <li class="nav-item">
-            <a href="./komite_disiplin_mahasiswa.html" class="nav-link">
-                <i class="far fa-circle nav-icon"></i>
-                <p>Komite Disiplin Mahasiswa</p>
-            </a>
+          <a href="./komite_disiplin_mahasiswa.html" class="nav-link">
+            <i class="far fa-circle nav-icon"></i>
+            <p>Komite Disiplin Mahasiswa</p>
+          </a>
         </li>
         <li class="nav-item">
-            <a href="./kps.html" class="nav-link">
-                <i class="far fa-circle nav-icon"></i>
-                <p>KPS</p>
-            </a>
+          <a href="./kps.html" class="nav-link">
+            <i class="far fa-circle nav-icon"></i>
+            <p>KPS</p>
+          </a>
         </li>
         <li class="nav-item">
-            <a href="index.php?page=data-mahasiswa" class="nav-link">
-                <i class="far fa-circle nav-icon"></i>
-                <p>Mahasiswa</p>
-            </a>
+          <a href="index.php?page=data-mahasiswa" class="nav-link">
+            <i class="far fa-circle nav-icon"></i>
+            <p>Mahasiswa</p>
+          </a>
         </li>
         <li class="nav-item">
-            <a href="./notification.html" class="nav-link">
-                <i class="far fa-circle nav-icon"></i>
-                <p>Notification</p>
-            </a>
+          <a href="./notification.html" class="nav-link">
+            <i class="far fa-circle nav-icon"></i>
+            <p>Notification</p>
+          </a>
         </li>
         <li class="nav-item">
-            <a href="./pelanggaran_mahasiswa.html" class="nav-link">
-                <i class="far fa-circle nav-icon"></i>
-                <p>Pelanggaran Mahasiswa</p>
-            </a>
+          <a href="./pelanggaran_mahasiswa.html" class="nav-link">
+            <i class="far fa-circle nav-icon"></i>
+            <p>Pelanggaran Mahasiswa</p>
+          </a>
         </li>
         <li class="nav-item">
-            <a href="./pelapor.html" class="nav-link">
-                <i class="far fa-circle nav-icon"></i>
-                <p>Pelapor</p>
-            </a>
+          <a href="./pelapor.html" class="nav-link">
+            <i class="far fa-circle nav-icon"></i>
+            <p>Pelapor</p>
+          </a>
         </li>
         <li class="nav-item">
-            <a href="./reward_archive.html" class="nav-link">
-                <i class="far fa-circle nav-icon"></i>
-                <p>Reward Archive</p>
-            </a>
+          <a href="./reward_archive.html" class="nav-link">
+            <i class="far fa-circle nav-icon"></i>
+            <p>Reward Archive</p>
+          </a>
         </li>
         <li class="nav-item">
-            <a href="./reward_points.html" class="nav-link">
-                <i class="far fa-circle nav-icon"></i>
-                <p>Reward Points</p>
-            </a>
+          <a href="./reward_points.html" class="nav-link">
+            <i class="far fa-circle nav-icon"></i>
+            <p>Reward Points</p>
+          </a>
         </li>
         <li class="nav-item">
-            <a href="./tenaga_kependidikan.html" class="nav-link">
-                <i class="far fa-circle nav-icon"></i>
-                <p>Tenaga Kependidikan</p>
-            </a>
+          <a href="./tenaga_kependidikan.html" class="nav-link">
+            <i class="far fa-circle nav-icon"></i>
+            <p>Tenaga Kependidikan</p>
+          </a>
         </li>
         <li class="nav-item">
-            <a href="./users.html" class="nav-link">
-                <i class="far fa-circle nav-icon"></i>
-                <p>Users</p>
-            </a>
+          <a href="./users.html" class="nav-link">
+            <i class="far fa-circle nav-icon"></i>
+            <p>Users</p>
+          </a>
         </li>
         <li class="nav-item">
-            <a href="./violation_level.html" class="nav-link">
-                <i class="far fa-circle nav-icon"></i>
-                <p>Violation Level</p>
-            </a>
+          <a href="./violation_level.html" class="nav-link">
+            <i class="far fa-circle nav-icon"></i>
+            <p>Violation Level</p>
+          </a>
         </li>
         <li class="nav-item">
-            <a href="./violation_report.html" class="nav-link">
-                <i class="far fa-circle nav-icon"></i>
-                <p>Violation Report</p>
-            </a>
+          <a href="./violation_report.html" class="nav-link">
+            <i class="far fa-circle nav-icon"></i>
+            <p>Violation Report</p>
+          </a>
         </li>
         <li class="nav-item">
-            <a href="./violation_type.html" class="nav-link">
-                <i class="far fa-circle nav-icon"></i>
-                <p>Violation Type</p>
-            </a>
+          <a href="./violation_type.html" class="nav-link">
+            <i class="far fa-circle nav-icon"></i>
+            <p>Violation Type</p>
+          </a>
         </li>
-    </ul>
-</li>
+      </ul>
+    </li>
+    <li class="nav-item ">
+      <a href="logout.php" class="nav-link text-red">
+        <i class="nav-icon fas fa-power-off"></i>
+        <p>
+          Logout
+          <!-- <span class="right badge badge-danger">New</span> -->
+        </p>
+      </a>
+    </li>
 
-     
-        </ul>
-      </nav>
+  </ul>
+</nav>
