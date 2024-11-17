7a11084980c86f6485978d04e0dfe163897a49ed
safrizalrahman46 <safrizalrahman46@gmail.com>
2024-11-17 21:57:21 +0800

    add SCRIPT SWEET ALERT

--- app/footer.php
+++ app/footer.php
@@ -42,6 +42,10 @@
 <!-- AdminLTE dashboard demo (This is only for demo purposes) -->
 <!-- <script src="dist/js/pages/dashboard.js"></script> -->
 
+<!-- SweetAlert2 -->
+<script src="plugins/sweetalert2/sweetalert2.min.js"></script>
+
+
 <!-- DataTables  & Plugins -->
 <script src="plugins/datatables/jquery.dataTables.min.js"></script>
 <script src="plugins/datatables-bs4/js/dataTables.bootstrap4.min.js"></script>
