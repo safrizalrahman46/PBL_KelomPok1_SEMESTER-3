b8d6f63215b5b6e0c993ccb12ab634cb07308d56
safrizalrahman46 <safrizalrahman46@gmail.com>
2024-11-18 08:10:16 +0700

    value="<?php echo $view['id'];?>value="<?php echo $view['id'];?>
    MENAMBAHKAN CODE ITU UNTUK BIDA MELIHAT DI EDIT

--- app/edit/edit_data.php
+++ app/edit/edit_data.php
@@ -1,9 +1,15 @@
+<?php
+$id = $_GET['id'];
+$query = mysqli_query($koneksi, "SELECT * FROM mahasiswa WHERE id='$id'");
+$view = mysqli_fetch_array($query);
+?>
+
 <section class="content">
 <div class="container-fluid">
    <!-- general form elements disabled -->
    <div class="card card-warning">
    <div class="card-header">
-                <h3 class="card-title">General Elements</h3>
+                <h3 class="card-title">Edit Data Mahaiswa</h3>
               </div>
               <!-- /.card-header -->
               <div class="card-body">
@@ -12,8 +18,8 @@
                     <div class="col-sm-6">
                       <!-- text input -->
                       <div class="form-group">
-                        <label>ID</label>
-                        <input type="text" name="id" class="form-control" placeholder="Enter ID">
+                        <label>Nomor</label>
+                        <input type="text" name="id" class="form-control" placeholder="Enter ID" value="<?php echo $view['id'];?>">
                       </div>
                     </div>
                     <div class="col-sm-6">
