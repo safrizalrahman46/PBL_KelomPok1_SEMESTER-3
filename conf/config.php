4979bf3a782ff52e8c728b510e550754c4a8b3f7
safrizalrahman46 <safrizalrahman46@gmail.com>
2024-11-18 07:50:56 +0700

    menambahkan agr bisa mengambil data dari tabel departement

--- conf/config.php
+++ conf/config.php
@@ -7,6 +7,17 @@
 // $koneksi = new mysqli($servername, $username, $password, $dbname);
 $koneksi = mysqli_connect('localhost', 'root', '', 'tatib3');
 
+// Fetch departments from the database
+$departments = [];
+$sql = "SELECT id, name FROM department";
+$result = mysqli_query($koneksi, $sql);
+
+if (mysqli_num_rows($result) > 0) {
+    while ($row = mysqli_fetch_assoc($result)) {
+        $departments[] = $row;
+    }
+}
+
 // if ($koneksi->connect_error) {
 //     die("Connection failed: " . $koneksi->connect_error);
 // }
@@ -15,4 +26,6 @@ $koneksi = mysqli_connect('localhost', 'root', '', 'tatib3');
 // }
 
 // $sql = "SELECT * FROM users
+
+
 ?>
