ece939ff28e1ccbe594b4f2590d11261552a7173
safrizalrahman46 <safrizalrahman46@gmail.com>
2024-12-03 12:48:56 +0700

    push

--- source/action/jurusanAction.php
+++ source/action/jurusanAction.php
@@ -7,30 +7,49 @@ if ($session->get('is_login') !== true) {
     header('Location: login.php');
 }
 
-include_once('../model/FakultasModel.php');
+include_once('../model/JurusanModel.php');
 include_once('../lib/Secure.php');
 
 $act = isset($_GET['act']) ? strtolower($_GET['act']) : '';
 
+
 if ($act == 'load') {
-    $fakultas = new FakultasModel();
-    $data = $fakultas->getData();
+    $jurusan = new JurusanModel();
+    $data = $jurusan->getData();
     $result = [];
     $i = 1;
     foreach ($data as $row) {
-        $result['data'][] = [
+        $result[] = [
             $i,
-            $row['name'],
-            '<button class="btn btn-sm btn-warning" onclick="editData(' . $row['id'] . ')"><i class="fa fa-edit"></i></button>  
-             <button class="btn btn-sm btn-danger" onclick="deleteData(' . $row['id'] . ')"><i class="fa fa-trash"></i></button>'
+            $row['nama_jurusan'],
+            $row['department_id'],
+            '<button class="btn btn-sm btn-warning" onclick="editData(' . $row['id_jurusan'] . ')"><i class="fa fa-edit"></i></button>  
+             <button class="btn btn-sm btn-danger" onclick="deleteData(' . $row['id_jurusan'] . ')"><i class="fa fa-trash"></i></button>'
         ];
         $i++;
     }
-    echo json_encode($result);
+    // echo json_encode($result);
+    echo json_encode(['data' => $result]); // Wrap the result in a 'data' key
 }
 // if ($act == 'load') {
-//     $fakultas = new FakultasModel();
-//     $data = $fakultas->getData();
+//     $jurusan = new JurusanModel();
+//     $data = $jurusan->getData();
+//     $result = [];
+//     $i = 1;
+//     foreach ($data as $row) {
+//         $result['data'][] = [
+//             $i,
+//             $row['name'],
+//             '<button class="btn btn-sm btn-warning" onclick="editData(' . $row['id_jurusan'] . ')"><i class="fa fa-edit"></i></button>  
+//              <button class="btn btn-sm btn-danger" onclick="deleteData(' . $row['id_jurusan'] . ')"><i class="fa fa-trash"></i></button>'
+//         ];
+//         $i++;
+//     }
+//     echo json_encode($result);
+// }
+// if ($act == 'load') {
+//     $jurusan = new JurusanModel();
+//     $data = $jurusan->getData();
 //     $result = ['data' => []];
 //     $i = 1;
 //     foreach ($data as $row) {
@@ -51,17 +70,18 @@ if ($act == 'load') {
 if ($act == 'get') {
     $id = (isset($_GET['id']) && ctype_digit($_GET['id'])) ? $_GET['id'] : 0;
 
-    $fakultas = new FakultasModel();
-    $data = $fakultas->getDataById($id);
+    $jurusan = new JurusanModel();
+    $data = $jurusan->getDataById($id);
     echo json_encode($data);
 }
 
 if ($act == 'save') {
     $data = [
-        'name' => antiSqlInjection($_POST['name'])
+        'nama_jurusan' => antiSqlInjection($_POST['nama_jurusan']),
+        'department_id' => antiSqlInjection($_POST['department_id'])
     ];
-    $fakultas = new FakultasModel();
-    $fakultas->insertData($data);
+    $jurusan = new JurusanModel();
+    $jurusan->insertData($data);
 
     echo json_encode([
         'status' => true,
@@ -72,11 +92,12 @@ if ($act == 'save') {
 if ($act == 'update') {
     $id = (isset($_GET['id']) && ctype_digit($_GET['id'])) ? $_GET['id'] : 0;
     $data = [
-        'name' => antiSqlInjection($_POST['name'])
+        'nama_jurusan' => antiSqlInjection($_POST['nama_jurusan']),
+        'department_id' => antiSqlInjection($_POST['department_id'])
     ];
 
-    $fakultas = new FakultasModel();
-    $fakultas->updateData($id, $data);
+    $jurusan = new JurusanModel();
+    $jurusan->updateData($id, $data);
 
     echo json_encode([
         'status' => true,
@@ -87,8 +108,8 @@ if ($act == 'update') {
 if ($act == 'delete') {
     $id = (isset($_GET['id']) && ctype_digit($_GET['id'])) ? $_GET['id'] : 0;
 
-    $fakultas = new FakultasModel();
-    $fakultas->deleteData($id);
+    $jurusan = new JurusanModel();
+    $jurusan->deleteData($id);
 
     echo json_encode([
         'status' => true,
