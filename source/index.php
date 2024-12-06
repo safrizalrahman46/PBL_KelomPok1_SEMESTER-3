d07fd7473a928f851d9ecab35eeef96a3690ca0c
safrizalrahman46 <safrizalrahman46@gmail.com>
2024-12-07 01:02:17 +0700

    fix menu done

--- source/index.php
+++ source/index.php
@@ -4,6 +4,7 @@ $session = new Session();
 if ($session->get('is_login') !== true) {
     header('Location: login.php');
 }
+
 ?>
 
 
--- source/layouts/sidebar.php
+++ source/layouts/sidebar.php
@@ -11,100 +11,159 @@
     </div>
     <!-- SidebarSearch Form -->
     <div class="form-inline">
-        <div class="input-group" data-widget="sidebar-search">
+        <!-- <div class="input-group" data-widget="sidebar-search">
             <input class="form-control form-control-sidebar" type="search"
                 placeholder="Search" aria-label="Search">
             <div class="input-group-append"><button class="btn btn-sidebar"><i class="fas fa-search fa-fw"></i></button></div>
-        </div>
+        </div> -->
     </div>
     <!-- Sidebar Menu -->
     <nav class="mt-2">
-    <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
-        <li class="nav-item">
-            <a href="index.php" class="nav-link">
-                <i class="nav-icon fas fa-tachometer-alt"></i>
-                <p>Dashboard</p>
-            </a>
-        </li>
-        <li class="nav-item">
-            <a href="index.php?page=admin" class="nav-link">
-                <i class="nav-icon fas fa-user-shield"></i>
-                <p>Admin</p>
-            </a>
-        </li>
-        <li class="nav-item">
-            <a href="index.php?page=log" class="nav-link">
-                <i class="nav-icon fas fa-user-shield"></i>
-                <p>Log</p>
-            </a>
-        </li>
-        <li class="nav-item">
-            <a href="index.php?page=dosen" class="nav-link">
-                <i class="nav-icon fas fa-chalkboard-teacher"></i>
-                <p>Dosen</p>
-            </a>
-        </li>
-        <li class="nav-item">
-            <a href="index.php?page=jenis_pelanggaran" class="nav-link">
-                <i class="nav-icon fas fa-exclamation-circle"></i>
-                <p>Jenis Pelanggaran</p>
-            </a>
-        </li>
-        <li class="nav-item">
-            <a href="index.php?page=tingkat_pelanggaran" class="nav-link">
-                <i class="nav-icon fas fa-exclamation-circle"></i>
-                <p>Tingkat Pelanggaran</p>
-            </a>
-        </li>
-        <li class="nav-item">
-            <a href="index.php?page=lapor_pelanggaran_mahasiswa" class="nav-link">
-                <i class="nav-icon fas fa-file-alt"></i>
-                <p>Lapor Pelanggaran Mahasiswa</p>
-            </a>
-        </li>
-        <li class="nav-item">
-            <a href="index.php?page=kelas" class="nav-link">
-                <i class="nav-icon fas fa-file-alt"></i>
-                <p>Kelas</p>
-            </a>
-        </li>
-        <li class="nav-item">
-            <a href="index.php?page=mahasiswa" class="nav-link">
-                <i class="nav-icon fas fa-user-graduate"></i>
-                <p>Mahasiswa</p>
-            </a>
-        </li>
-        <li class="nav-item">
-            <a href="index.php?page=notifikasi" class="nav-link">
-                <i class="nav-icon fas fa-bell"></i>
-                <p>Notifikasi</p>
-            </a>
-        </li>
-        <li class="nav-item">
-            <a href="index.php?page=tipe_notifikasi" class="nav-link">
-                <i class="nav-icon fas fa-bell"></i>
-                <p>Tipe Notifikasi</p>
-            </a>
-        </li>
-        <li class="nav-item">
-            <a href="index.php?page=prodi" class="nav-link">
-                <i class="nav-icon fas fa-graduation-cap"></i>
-                <p>Prodi</p>
-            </a>
-        </li>
-        <li class="nav-item">
-            <a href="index.php?page=users" class="nav-link">
-                <i class="nav-icon fas fa-user-tie"></i>
-                <p>Users</p>
-            </a>
-        </li>
-        <li class="nav-item">
-            <a href="action/auth.php?act=logout" class="nav-link">
-                <i class="nav-icon fas fa-sign-out-alt"></i>
-                <p>Logout</p>
-            </a>
-        </li>
-    </ul>
-</nav>
+        <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
+            <li class="nav-item">
+                <a href="index.php" class="nav-link">
+                    <i class="nav-icon fas fa-tachometer-alt"></i>
+                    <p>Dashboard</p>
+                </a>
+            </li>
+
+
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                    <i class="nav-icon fas fa-copy"></i>
+                    <p>
+                        Master Data
+                        <i class="fas fa-angle-left right"></i>
+                        <!-- <span class="badge badge-info right">6</span> -->
+                    </p>
+                </a>
+                <ul class="nav nav-treeview">
+
+
+
+                    <li class="nav-item">
+                        <a href="index.php?page=prodi" class="nav-link">
+                            <i class="nav-icon fas fa-graduation-cap"></i>
+                            <p>Prodi</p>
+                        </a>
+                    </li>
+
+
+                    <li class="nav-item">
+                        <a href="index.php?page=kelas" class="nav-link">
+                            <i class="nav-icon fas fa-file-alt"></i>
+                            <p>Kelas</p>
+                        </a>
+                    </li>
+
+
+                    <li class="nav-item">
+                        <a href="index.php?page=jenis_pelanggaran" class="nav-link">
+                            <i class="nav-icon fas fa-exclamation-circle"></i>
+                            <p>Jenis Pelanggaran</p>
+                        </a>
+                    </li>
+                    <li class="nav-item">
+                        <a href="index.php?page=tingkat_pelanggaran" class="nav-link">
+                            <i class="nav-icon fas fa-exclamation-circle"></i>
+                            <p>Tingkat Pelanggaran</p>
+                        </a>
+                    </li>
+
+
+                    <li class="nav-item">
+                        <a href="index.php?page=tipe_notifikasi" class="nav-link">
+                            <i class="nav-icon fas fa-bell"></i>
+                            <p>Tipe Notifikasi</p>
+                        </a>
+                    </li>
+
+
+
+
+                    <li class="nav-item">
+                        <a href="index.php?page=notifikasi" class="nav-link">
+                            <i class="nav-icon fas fa-bell"></i>
+                            <p>Notifikasi</p>
+                        </a>
+                    </li>
+
+
+                </ul>
+            </li>
+
+
+            <li class="nav-item">
+                <a href="#" class="nav-link">
+                    <i class="nav-icon fas fa-user"></i>
+                    <p>
+                        Data Akun
+                        <i class="fas fa-angle-left right"></i>
+                        <!-- <span class="badge badge-info right">6</span> -->
+                    </p>
+                </a>
+                <ul class="nav nav-treeview">
+
+                    <li class="nav-item">
+                        <a href="index.php?page=admin" class="nav-link">
+                            <i class="nav-icon fas fa-user-shield"></i>
+                            <p>Admin</p>
+                        </a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a href="index.php?page=mahasiswa" class="nav-link">
+                            <i class="nav-icon fas fa-user-graduate"></i>
+                            <p>Mahasiswa</p>
+                        </a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a href="index.php?page=dosen" class="nav-link">
+                            <i class="nav-icon fas fa-chalkboard-teacher"></i>
+                            <p>Dosen</p>
+                        </a>
+                    </li>
+
+                    <li class="nav-item">
+                        <a href="index.php?page=log" class="nav-link">
+                            <i class="nav-icon fas fa-user-shield"></i>
+                            <p>Log</p>
+                        </a>
+                    </li>
+
+                    <!-- <li class="nav-item">
+                        <a href="index.php?page=users" class="nav-link">
+                            <i class="nav-icon fas fa-user-tie"></i>
+                            <p>Users</p>
+                        </a>
+                    </li> -->
+                </ul>
+            </li>
+
+
+
+
+
+            <li class="nav-item">
+                <a href="index.php?page=lapor_pelanggaran_mahasiswa" class="nav-link">
+                    <i class="nav-icon fas fa-file-alt"></i>
+                    <p>Lapor Pelanggaran </p>
+                </a>
+            </li>
+
+
+
+
+
+
+            <li class="nav-item">
+                <a href="action/auth.php?act=logout" class="nav-link">
+                    <i class="nav-icon fas fa-sign-out-alt"></i>
+                    <p>Logout</p>
+                </a>
+            </li>
+        </ul>
+    </nav>
     <!-- /.sidebar-menu -->
 </div>
